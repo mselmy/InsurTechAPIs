948dd2748883448c8f4f873b113ec2cb30b65b0d
AsmaaAlbhbity <asmaa.albhbity@gmail.com>
2024-06-13 16:45:59 +0300

    Login

--- InsurTechSolution/InsurTech.APIs/Controllers/AccountController.cs
+++ InsurTechSolution/InsurTech.APIs/Controllers/AccountController.cs
@@ -11,6 +11,35 @@ namespace InsurTech.APIs.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        #region Login
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO model)
+        {
+            var User = await _userManager.FindByEmailAsync(model.Email);
+            if (User is null) return Unauthorized(new ApiResponse(401));
+
+            var Result = await _signInManager.CheckPasswordSignInAsync(User, model.Password, false);
+
+            if (!Result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return Ok(new UserDTO()
+            {
+                Id = User.Id,
+                Email = User.Email,
+                Name = User.UserName,
+                Token = "hi"
+            });
+
+        }
+
+        #endregion
     }
 }
