c74bf94ec9aefd94e523f088efa94a1ecb62e426
AsmaaAlbhbity <asmaa.albhbity@gmail.com>
2024-06-16 15:19:31 +0300

    sold insurance for company

--- InsurTechSolution/InsurTech.APIs/Controllers/InsurancePlanController.cs
+++ InsurTechSolution/InsurTech.APIs/Controllers/InsurancePlanController.cs
@@ -3,12 +3,15 @@ using Google.Apis.Auth.OAuth2;
 using InsurTech.APIs.DTOs;
 using InsurTech.APIs.DTOs.HealthInsurancePlanDTO;
 using InsurTech.APIs.DTOs.HomeInsurancePlanDTO;
+using InsurTech.APIs.DTOs.InsurancePlanDTO;
 using InsurTech.APIs.DTOs.MotorInsurancePlanDTO;
 using InsurTech.APIs.Errors;
 using InsurTech.Core;
 using InsurTech.Core.Entities;
+using InsurTech.Core.Entities.Identity;
 using InsurTech.Repository;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InsurTech.APIs.Controllers
@@ -19,11 +22,13 @@ namespace InsurTech.APIs.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly UserManager<AppUser> _userManager;
 
-        public InsurancePlanController(IUnitOfWork unitOfWork , IMapper mapper)
+        public InsurancePlanController(IUnitOfWork unitOfWork , IMapper mapper , UserManager<AppUser> userManager)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _userManager = userManager;
         }
 
         [HttpDelete("DeleteInsurancePlan/{id}")]
@@ -125,6 +130,70 @@ namespace InsurTech.APIs.Controllers
             catch (Exception)
             {
                 return StatusCode(500, new ApiResponse(500, "An error occurred while retrieving insurance plans."));
+            }
+        }
+        [HttpGet("SoldInsuranceByCompanyId/{companyId}")]
+        public async Task<IActionResult> SoldInsuranceByCompanyId(string companyId)
+        {
+            try
+            {
+                var companyInsurancePlans = await _unitOfWork.Repository<InsurancePlan>().GetAllAsync();
+                var insurancePlansForCompany = companyInsurancePlans.Where(plan => plan.CompanyId == companyId).ToList();
+
+                if (!insurancePlansForCompany.Any())
+                {
+                    return NotFound(new ApiResponse(404, "No insurance plans found for the specified company."));
+                }
+
+                var insurancePlanIds = insurancePlansForCompany.Select(plan => plan.Id).ToList();
+
+                var userRequests = await _unitOfWork.Repository<UserRequest>().GetAllAsync();
+                var soldInsuranceRequests = userRequests.Where(request => insurancePlanIds.Contains(request.InsurancePlanId)).ToList();
+
+                if (!soldInsuranceRequests.Any())
+                {
+                    return NotFound(new ApiResponse(404, "No sold insurance found for the specified company."));
+                }
+
+                var userIds = soldInsuranceRequests.Select(request => request.CustomerId).Distinct().ToList();
+
+                // Dictionary to hold usernames for each insurance plan
+                var insurancePlanUsernames = new Dictionary<int, List<string>>();
+
+                foreach (var insurancePlan in insurancePlansForCompany)
+                {
+                    var usernames = new List<string>();
+                    var userRequestsForPlan = soldInsuranceRequests.Where(request => request.InsurancePlanId == insurancePlan.Id).ToList();
+
+                    foreach (var request in userRequestsForPlan)
+                    {
+                        var user = await _userManager.FindByIdAsync(request.CustomerId);
+                        if (user != null)
+                        {
+                            usernames.Add(user.UserName);
+                        }
+                    }
+
+                    insurancePlanUsernames[insurancePlan.Id] = usernames;
+                }
+
+                var result = insurancePlansForCompany.Select(plan => new InsurancePlanAndUserNameDTO
+                {
+                    InsurancePlan = new InsurancePlanForCompanyDTO
+                    {
+                        CategoryName=plan.Category.Name,
+                        Level=plan.Level,
+                        Quotation=plan.Quotation,
+
+                    },
+                    Usernames = insurancePlanUsernames[plan.Id]
+                });
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "Internal server error. Please try again later.");
             }
         }
 
---
+++ InsurTechSolution/InsurTech.APIs/DTOs/InsurancePlanDTO/InsurancePlanAndUserNameDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace InsurTech.APIs.DTOs.InsurancePlanDTO
+{
+    public class InsurancePlanAndUserNameDTO
+    {
+        public InsurancePlanForCompanyDTO? InsurancePlan { get; set; }
+        public List<string>? Usernames { get; set; }
+    }
+}
