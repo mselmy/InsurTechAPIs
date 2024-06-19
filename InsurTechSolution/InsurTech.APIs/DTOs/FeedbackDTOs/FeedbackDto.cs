aa62d92050b3531f3a29c58a6f883b8b658fa138
EbrahimDawoud <ebrahim_EN30895331@f-eng.tanta.edu.eg>
2024-06-19 23:55:27 +0300

    feat: add FeedbackDTOs

---
+++ InsurTechSolution/InsurTech.APIs/DTOs/FeedbackDTOs/FeedbackDto.cs
@@ -0,0 +1,12 @@
+﻿using InsurTech.Core.Entities;
+
+public class FeedbackDto
+{
+    public int Id { get; set; }
+    public Rating Rating { get; set; }
+    public string Comment { get; set; }
+    public string CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public int InsurancePlanId { get; set; }
+    public string InsurancePlanName { get; set; }
+}
---
+++ InsurTechSolution/InsurTech.APIs/DTOs/FeedbackDTOs/FeedbackMapProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using InsurTech.APIs.DTOs.FeedbackDTOs;
+using InsurTech.Core.Entities;
+
+public class FeedbackMapProfile : Profile
+{
+    public FeedbackMapProfile()
+    {
+        CreateMap<Feedback, FeedbackDto>().ReverseMap();
+        CreateMap<CreateFeedbackInput, Feedback>().ReverseMap();
+    }
+}
